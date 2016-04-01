70d174f4f5a3ff7968591ab62f3c0cf95b5a92b1
jbabu <jbabu@ccs.neu.edu>
2016-03-31 23:06:20 -0400

    Changes to server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 console.log("secret");
-console.log(process.env.PASSPORT_SECRET);
+//console.log(process.env.PASSPORT_SECRET);
 
 
 //NOTE: PLease keep this before all http calls
